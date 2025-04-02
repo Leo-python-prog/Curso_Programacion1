7a10680033ddb18982f10361e63d223611ace7b0
leo <leoanrdomule@gmail.com>
2025-04-02 10:47:11 -0300

    Solución a los primeros ejercicios

---
+++ Proyectos/sumar_números.py
@@ -0,0 +1,14 @@
+a = 20
+b = 25
+suma = a + b
+
+print(suma)
+
+# Otra forma de sumar dos valores
+
+def sumar(a,b):
+    return a + b
+
+resultado = sumar(201, 250)
+
+print(resultado)
---
+++ Proyectos/área_de_un_rectángulo.py
@@ -0,0 +1,9 @@
+ancho = 20
+largo = 25
+titulo = "Vamos a calcular el área de un rectángulo"
+texto = f"El ancho de nuestro rectángulo es de {ancho} cm. y el largo es de {largo} cm."
+resultado = ancho * largo
+
+print(titulo)
+print(texto)
+print(f"El área de nuestro rectángulo es de {resultado} cm.")
